f592c92fc0b0af2e908de0dbb00da64a4db904b8
Семья <kaloshaviktar1@gmail.com>
2022-11-04 21:26:01 +0100

    lesson 5 function

---
+++ Frontend/Tasks FE nelli/Lessons_6_function/script/script.js
@@ -0,0 +1,81 @@
+    // Создайте функцию, которая принимает в качестве аргумента строку и выводит каждый символ этой строки в консоль.
+
+      //   function showEachLetter(str){
+      //   for(let i = 0; i < str.length; i++){
+      //     console.log(str[i])
+      //   }
+      // }
+      //  showEachLetter('hello'); // h, e, l, l, o
+  
+
+
+
+  //___________________________________________________
+  // Дан массив с числовыми значениями
+  // Написать функцию, при вызове которой выводятся в консоль все элементы массива
+  
+  // let numbers = [1, 5, 88, 4, -20, -4, 57];
+  // function showEachElement(arr){
+  //   for(let i = 0; i < arr.length; i++){
+  //     console.log(arr[i])
+  //   }
+  // }
+  //   showEachElement(numbers);
+  
+
+
+
+  //___________________________________________________
+    // Дан массив с числовыми значениями
+    // Написать функцию, при вызове которой выводятся в консоль все отрицательные элементы массива 
+  
+  // let numbers = [1, 5, 88, 4, -20, -4, 57];
+  // let numbers_2 = [4, 50000, -4];
+  // function showEachNegativeElement(arr){
+  //   for(let i = 0; i < arr.length; i++){
+  //     if(arr[i] < 0){
+  //       console.log(arr[i])
+  //     }
+  //   }
+  // }
+  
+  //  showEachNegativeElement(numbers);
+  //  console.log('------------------------')
+  //  showEachNegativeElement(numbers_2);
+  
+
+
+
+  //___________________________________________________
+  // Написать функцию, при вызове которой выводится максимальное число из перечисленных
+  // let numbers = [1, 5, 88, 4, -20, -4, 57];
+  
+  // function showMaxNum(arr){
+  //   let max = arr[0]; // 1
+  
+  //   for(let i = 1; i < arr.length; i++){
+  //     if(arr[i] > max){
+  //       max = arr[i] // max = 5, max = 88
+  //     }
+  //   }
+  //   console.log(max);
+  // }
+  // showMaxNum (numbers)
+  
+
+
+
+
+  //___________________________________________________
+  // Написать функцию, при вызове которой выводятся все четные положительные числа 
+  // let numbers = [1, 5, 88, 4, -20, -4, 57];
+  
+  // function func(arr){
+  //   for(let i = 0; i < arr.length; i++){
+  //     if(arr[i] > 0 && arr[i] % 2 === 0){
+  //       console.log(arr[i])
+  //     }
+  //   }
+  // }
+  // func(numbers);
+  
