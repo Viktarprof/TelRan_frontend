d21e237cf63a2d7dcde8f6bb71fa571cd58a63e3
Семья <kaloshaviktar1@gmail.com>
2022-11-16 14:48:49 +0100

    Nelli lesson2

---
+++ Frontend/Tasks FE nelli/Profcours/lessons2/script/script.js
@@ -0,0 +1,97 @@
+// 1. В div с классом cards_container создать карточку с тремя параграфами - First name: Alex, Last name: Ivanov, Age: 47
+// const cards_container = document.querySelector('.cards_container');
+
+// const cards = document.createElement('div');
+
+// const firsttame = document.createElement('p');
+// const lastName = document.createElement('p');
+// const age = document.createElement('p');
+
+// cards_container.append(firstName, lastName, age)
+
+// firstName.innerText = 'First name: Alex', 
+// lastName.innerText = 'Last name: Ivanov', 
+// age.innerText = 'Age: 47';
+//     cards_container.append(firstName, lastName, age)
+
+
+
+
+
+
+
+
+// 2. Добавить элементу card класс cards. По этому классу в стилях добавить свойства: width, border, border-radius, padding, margin, background-color
+
+// 3. Параграфу ageElem задать размер шрифта 24px (сделать это через JS)
+
+
+// const cards_container = document.querySelector('.cards_container');
+// const cards = document.createElement('div');
+
+//     const firstName = document.createElement('p');
+//     const lastName = document.createElement('p');
+//     const age = document.createElement('p');
+
+//     firstName.innerText = 'First name: Alex', 
+//     lastName.innerText = 'Last name: Ivanov', 
+//     age.innerText = 'Age: 47';
+
+//     cards.classList.add('card');
+//     age.style.fontSize = '24px';
+
+//     cards.append(firstName, lastName, age)
+//     cards_container.append(cards)
+
+
+//4. вывели на экран три одинаковые карточки через цикл
+// const name = 'Olga';
+// for (let threeCards of name){
+
+// const cards_container = document.querySelector('.cards_container');
+// const cards = document.createElement('div');
+
+//     const firstName = document.createElement('p');
+//     const lastName = document.createElement('p');
+//     const age = document.createElement('p');
+
+//     firstName.innerText = 'First name: Alex', 
+//     lastName.innerText = 'Last name: Ivanov', 
+//     age.innerText = 'Age: 47';
+
+//     cards.classList.add('card');
+//     age.style.fontSize = '24px';
+
+//     cards.append(firstName, lastName, age)
+//     cards_container.append(cards)
+// }
+
+
+
+
+
+
+
+
+// Вывести в консоль числа от 25 до 39, используя цикл for
+// count = 0;
+// sum = 0;
+// for(let i = 25; i <= 39; i++){
+//     count++
+//     sum += i
+
+//     console.log('Показываю все элементы = ' + i);
+  
+// } console.log('всего элементов проверенно в цикле ' + count);
+// console.log('сумма всех элементов ' + sum);
+
+
+// Вывести в консоль четные числа от 60 до 45
+count = 0;
+sum = 0;
+for(let i = 60; i >= 45; i--){
+    if (i % 2 === 0){    
+
+    console.log('Показываю все элементы = ' + i);
+    }
+}ки  
